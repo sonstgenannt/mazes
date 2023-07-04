3d6d8ab6c79c5975b9e93f28cff7e7577fcc9849
eugenekingfish <aojg@protonmail.ch>
2023-07-04 18:40:16 +0100

    added comments to graph.h

--- headers/graph.h
+++ headers/graph.h
@@ -5,7 +5,13 @@ class graph {
       std::vector<node> nodes;
 
    public:
-      graph();
-      bool add_node(const node& n);
-      std::vector<node> get_nodes();
+      graph(); // constructor
+
+      /*
+       * Adds a node to the graph. Nodes must be unique -- i.e. the same node cannot be added to a graph more than once.
+       * node -- the node to be added
+       */
+      bool add_node(const node& n); 
+
+      std::vector<node> get_nodes(); // getter for nodes vector
 };
